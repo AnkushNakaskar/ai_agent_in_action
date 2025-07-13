88f354497ae6ab01eb3f3dbcf114d97fb7e6a8cc
Ankush Nakaskar <ankush.nakaskar@phonepe.com>
2025-07-13 13:35:07 +0530

    Added working code for semantic kernel

--- chapter_05/semantic_kernel/semantic_kernel_execution.py
+++ chapter_05/semantic_kernel/semantic_kernel_execution.py
@@ -2,14 +2,19 @@ import asyncio
 import json
 import semantic_kernel as sk
 from readkey import get_token
-
+import httpx
+from openai import AsyncOpenAI
 from semantic_kernel.functions.kernel_function_decorator import kernel_function
 
 selected_service = "OpenAI"
 kernel = sk.Kernel()
 api_key = str(get_token())
 service_id = None
-
+custom_async_http_client = httpx.AsyncClient(verify=False)
+openai_client_with_custom_http = AsyncOpenAI(
+            api_key=api_key,
+            http_client=custom_async_http_client # Pass the custom httpx.AsyncClient here
+        )
 @kernel_function(name="recommend_fun", description="Get the weather for a city")
 def recommend_fun(topic, rating="good"):
     if "time travel" in topic.lower():     #1
@@ -38,15 +43,16 @@ if selected_service == "OpenAI":
             ai_model_id="gpt-4o-mini",
             api_key=api_key,
             org_id="org-MAd9CASqY83u8gtOXsyQP5Lx",
+            async_client=openai_client_with_custom_http
         ),
     )
 
 
 
-# This function is currently broken
 async def run_prompt():
     result = await kernel.invoke_prompt(prompt="recommend a movie about time travel",
                                         function_name="recommend_fun")
+    print("Result for sematic kernel is ::::")
     print(result)
 
 
